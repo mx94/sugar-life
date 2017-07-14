156ec31ab9ad1aafac8c6d86ffecd486f15b0844
mx94 <maxinohnono@hotmail.com>
2017-07-14 15:30:57 +0800

    'add'

--- src/components/Pay/Pay.vue
+++ src/components/Pay/Pay.vue
@@ -166,8 +166,9 @@
                                             "signType": wx_data.signType,
                                             "paySign": wx_data.paySign
                                         },
-                                        function (res) {
-                                            if (res.err_msg == "get_brand_wcpay_request:ok") {
+                                        function (wxres) {
+                                            alert(wxres.err_msg)
+                                            if (wxres.err_msg == "get_brand_wcpay_request:ok") {
                                                 // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回 ok，但并不保证它绝对可靠
                                                 this.$router.replace('/paysuccess/' + this.info.storeId)
                                             } else {
